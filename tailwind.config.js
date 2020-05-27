078b0bd1755e4110dc80447dd6a3be87f243dbd0
飯塚浩也 <hiroya0317@nms.ac.jp>
2020-05-27 15:57:17 +0900

    change postcss setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  purge: {
-    enabled: process.env.NODE_ENV === "production",
-    content: ["./src/**/*.tsx", "./src/**/*.ts"],
-  },
   theme: {
     //ex: position:relative left-2
     inset: {
